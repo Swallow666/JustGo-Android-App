f0c08a6ffe298f9f983563dc836241886efc10c9
zhaoxing2788 <zhaoxing2788@gmail>
2018-03-07 19:55:14 -0500

    Can be searching near by current location

--- app/src/main/java/com/example/liyuan/justgo/MainActivity.java
+++ app/src/main/java/com/example/liyuan/justgo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.liyuan.justgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton=(Button)findViewById(R.id.rbFind);
+        buttonClickListener listener=new buttonClickListener();
+
+        myButton.setOnClickListener(listener);
+    }
+
+    class  buttonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            intent.setClass(MainActivity.this,MapsActivity.class);
+            startActivity(intent);
+        }
     }
 }
